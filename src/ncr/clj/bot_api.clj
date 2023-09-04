3cfa4673f46f27a1764070738c65b49897e763cd
dmitry kim <jason@nichego.net>
2023-09-04 21:00:46 +0200

    support logging into a cluster

--- src/ncr/clj/bot_api.clj
+++ src/ncr/clj/bot_api.clj
@@ -14,6 +14,22 @@
 (defn make-client [cfg]
   {:cfg (merge DEFAULTS cfg) :cache (atom {})})
 
+(defn login-as [client {:keys [subject]}]
+  (assoc client :subject subject))
+
+(def Q_ENUMERATE_ROBOTS
+  "query($cluster: String) {
+     robots_enumerate(clusterSlug: $cluster) {
+       id, name, subject, active
+     }
+   }")
+
+(defn login-into [client cluster]
+  (let [resp (graphql ncr Q_ENUMERATE_ROBOTS {:cluster cluster})
+        [{:keys [subject]} :as robots] (get-in resp [:data :robots_enumerate])]
+    (assert (= 1 (count robots)))
+    (login-as client {:subject subject})))
+
 (defn- cache-set
   ([c k v] (cache-set c k v nil))
   ([cache k v expire]
@@ -80,11 +96,14 @@
       (cache-set cache :oidc token (.plusSeconds now (/ ttl 2)))
       token)))
 
-(defn graphql [client query & [vars]]
-  (http-req (get-in client [:cfg :neckar-url]) "/api/graphql"
-    {:method :post
-     :headers {"authorization" (str "Bearer " (oidc-token client))}
-     :body {:query query :variables vars}}))
+(defn graphql [{:keys [subject] :as client} query & [vars]]
+  (let [headers
+        (cond-> {"authorization" (str "Bearer " (oidc-token client))}
+          subject (assoc "X-Ncr-Account" subject))]
+    (http-req (get-in client [:cfg :neckar-url]) "/api/graphql"
+      {:method :post
+       :headers headers
+       :body {:query query :variables vars}})))
 
 (defn transit< [in]
   (let [in (java.io.ByteArrayInputStream. (.getBytes in))]
@@ -103,14 +122,31 @@
   (def ncr
     (-> "config.edn" slurp read-string make-client))
 
+  (def ncr
+    (-> "mail-import.dev.edn" slurp read-string make-client))
+
   (oidc-token ncr)
 
-  (def ^:private Q
+  (def ^:private Q_FIND_CLUSTER
     "query($slug: String) {
        default:cluster_find_by_slug(slug: $slug) {
          id, name
        }
      }")
 
-  (graphql ncr Q {:slug "suprematic"})
+  (graphql ncr Q_FIND_CLUSTER {:slug "suprematic"})
+
+  (def ncr-s
+    (login-into ncr "suprematic"))
+
+  (def ^:private Q_READ_PROFILE
+    "query($scope: String!) {
+        profile_account_read(scope: $scope)
+     }")
+
+  (-> ncr-s
+    (graphql Q_READ_PROFILE {:scope "robot/settings"})
+    (get-in [:data :profile_account_read])
+    transit<)
+
   )
